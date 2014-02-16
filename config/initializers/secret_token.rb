f32e96fc1b022f9a45fa0760f588bd751cc261cc
Yehuda Zargarov <yehuda@conduit.com>
2014-02-16 09:22:12 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codefuel::Application.config.secret_token = 'decb741ce27dedfd59a3ad85b37c9d50fc472cc8fabf3ad0abd7415070c95f5908208e9e2d911a0cbacb21ec5852231daf6cb61f708cdc19aa085647f81b0f2e'
